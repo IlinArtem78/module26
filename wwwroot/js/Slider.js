2db3b7ff4616a60dc1c8fe5b8c3c43d8d7d45798
IlinArtem78 <artem.ilin1994@gmail.com>
2025-02-21 09:08:40 +0300

    LifeSpot (HW-03) final ver

--- wwwroot/js/Slider.js
+++ wwwroot/js/Slider.js
@@ -1,4 +1,10 @@
-﻿
+﻿function showSlide(currentClass, nextClass) {
+    const current = document.querySelector(`.${currentClass}`);
+    const next = document.querySelector(`.${nextClass}`);
+
+    if (current) current.style.display = "none";
+    if (next) next.style.display = "block";
+}
 
 ///Навигация кнопками 
 //первый сладй ( с 1 на 2)
@@ -48,8 +54,9 @@ btn3prev.addEventListener('click', function (e) {
 
 
 
-//навигация с помощью движения мыши 
+//навигация с помощью движения мыши
 //слайд 1 (с 1 на 2)
+/*
 const pic1To2 = document.getElementsByClassName('first');
 pic1To2[0].addEventListener('wheel', function (e) {
     const hide1img = document.getElementsByClassName('img-first');
@@ -64,6 +71,8 @@ const pic2To1 = document.getElementsByClassName('second');
 pic2To1[0].addEventListener('wheel', function (e) {
     const hide2img = document.getElementsByClassName('img-second');
     hide2img[0].style.display = "none";
+
+
 
     if (document.getElementsByClassName('img-first').length > 0) {
         document.getElementsByClassName('img-first')[0].style.display = "block";
@@ -94,11 +103,18 @@ pic3to2[0].addEventListener('wheel', function (e) {
         document.getElementsByClassName('img-second')[0].style.display = "block";
     }
 });
-
-/*
-
-
-
-
-
 */
+
+
+
+document.querySelector('.first')?.addEventListener('wheel', () => showSlide('img-first', 'img-second'));
+document.querySelector('.second')?.addEventListener('wheel', (e) => {
+    if (e.deltaY > 0) {
+        showSlide('img-second', 'img-third'); // Вниз (вперёд)
+    } else {
+        showSlide('img-second', 'img-first'); // Вверх (назад)
+    }
+});
+document.querySelector('.third')?.addEventListener('wheel', () => showSlide('img-third', 'img-second'));
+
+
